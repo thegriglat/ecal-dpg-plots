cff67bdfb949b4876bc5e0044081115fbc9d059d
Grigory <thegriglat@gmail.com>
2020-08-20 21:57:57 +0300

    Fix typo

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -151,7 +151,7 @@ export class AppComponent {
       this.zoomLevel = 4;
     if (this.zoomLevel > MINIFY_LIMIT) this.minified = true;
     // minus zooming
-    if (this.zoomLevel < MINIFY_LIMIT - 1 && increment < 0) this.minified = false;
+    if (this.zoomLevel < (MINIFY_LIMIT + 1) && increment < 0) this.minified = false;
   }
 
   getZoomClass(): string {
