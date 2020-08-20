34f4593a34b46a00d1fa893df1246e36bb42d85e
Grigory <thegriglat@gmail.com>
2020-08-20 12:33:49 +0300

    Add filter

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,7 +16,8 @@ import { PlotCardComponent } from './plot-card/plot-card.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
