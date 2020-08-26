bd9ffb6ae609e5f1d26150c64b45ca31213b2e17
Grigory <thegriglat@gmail.com>
2020-08-26 14:42:22 +0300

    Fix ng build options

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 DATAFILE=site/src/data.json
 NG=node_modules/.bin/ng
 
-NGOPTS="--prod --base-href https://ecal.gitlab.io/ecaldpgplots/ --deploy-url https://ecal.gitlab.io/ecaldpgplots/"
+NGOPTS="--prod --base-href https://ecaldpgplots.web.cern.ch/ --deploy-url https://ecaldpgplots.web.cern.ch/"
 
 echo "Generating data file ..."
 python merge_content.py content > $DATAFILE
