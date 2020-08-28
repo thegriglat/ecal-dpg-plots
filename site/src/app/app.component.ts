7bb2f871397a6a37c21b9bf3d9c0dd409cfae424
Grigory <thegriglat@gmail.com>
2020-08-28 15:16:12 +0300

    Revert "Force https"
    This reverts commit 2d68f3d73f309a325938d32f7ed508ac465be13f.

--- site/src/app/app.component.ts
+++ site/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { environment } from 'src/environments/environment';
+import { Component } from '@angular/core';
 import { DataService } from './services/data.service';
 
 
@@ -8,15 +7,7 @@ import { DataService } from './services/data.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  ngOnInit(): void {
-    if (environment.production) {
-      if (location.protocol === 'http:') {
-        window.location.href = location.href.replace('http', 'https');
-      }
-    }
-  }
+export class AppComponent {
 
   constructor(private dataServ: DataService) { }
 
