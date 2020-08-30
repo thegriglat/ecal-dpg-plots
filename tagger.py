7dd04b1633cadb90c45f72d9e43686577fc98c48
Federico Ferri <federico.ferri@cern.ch>
2020-08-30 11:31:06 +0200

    force python3; list does not exclude tagging and is now done after all modifications, if any

--- tagger.py
+++ tagger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import shutil
@@ -19,7 +19,7 @@ A list of metadata files can be provided with the -f option or simply via stdin.
     parser.add_argument('-c', '--clear', action='store_true', default=False, help='clear the existing tags first')
     parser.add_argument('-f', '--file-list', nargs=1, help='file containing the list of metadata files to process')
     parser.add_argument('-i', '--in-place', action='store_true', default=False, help='edit file(s) in-place (makes backup if suffix option provided)')
-    parser.add_argument('-l', '--list', action='store_true', default=False, help='list tags (all other options are ignored)')
+    parser.add_argument('-l', '--list', action='store_true', default=False, help='list tags (after applied changes, if any)')
     parser.add_argument('-s', '--suffix', nargs=1, default=' ', help='suffix for the backup copy when in-place editing')
     parser.add_argument('-t', '--tagset', nargs=1, default='tags.yaml', help='reference tag list for checking and sorting tags')
     parser.add_argument('-v', '--verbose', action='store_true', default=False, help='verbose output')
@@ -127,11 +127,10 @@ if sys.stdin.isatty():
         fn = sys.argv[-1]
         if os.path.exists(fn):
             fd = open(fn)
+            process(fn, fd, tags, ref_tags, in_place=in_place, clear=args.clear)
+            fd.close()
             if args.list:
                 list_tags(fn, fd)
-            else:
-                process(fn, fd, tags, ref_tags, in_place=in_place, clear=args.clear)
-            fd.close()
         else:
             print(sys.argv[0] + ": cannot access '" + fn + "': No such file or directory.")
             sys.exit(1)
@@ -143,8 +142,7 @@ for fn in ifiles:
         print(fn)
     if os.path.exists(fn):
         fd = open(fn)
+        process(fn, fd, tags, ref_tags, in_place=in_place, clear=args.clear)
+        fd.close()
         if args.list:
             list_tags(fn, fd)
-        else:
-            process(fn, fd, tags, ref_tags, in_place=in_place, clear=args.clear)
-        fd.close()
