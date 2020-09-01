87353e2ca8d9915b110221b99fa16b92029aa380
Grigory <thegriglat@gmail.com>
2020-09-02 02:01:24 +0300

    Restyle message

--- site/src/app/plot-card/plot-permalink/plot-permalink.component.ts
+++ site/src/app/plot-card/plot-permalink/plot-permalink.component.ts
@@ -1,22 +1,28 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { IPopup } from '@richardlt/ng2-semantic-ui';
+import { Animations } from './../../classes/animation';
 
 @Component({
   selector: 'app-plot-permalink',
   templateUrl: './plot-permalink.component.html',
-  styleUrls: ['./plot-permalink.component.css']
+  styleUrls: ['./plot-permalink.component.css'],
+  animations: [
+    Animations.verticalSlideDown
+  ]
 })
 export class PlotPermalinkComponent implements OnInit {
 
   @Input() url = '';
+  visible = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  public showCopiedMessage(popup: IPopup): void {
-    popup.open();
-    setTimeout(() => popup.close(), 3000);
+  public showCopiedMessage(): void {
+    this.visible = true;
+    setTimeout(() => {
+      this.visible = false;
+    }, 3000);
   }
 
   fullurl(url: string): string {
