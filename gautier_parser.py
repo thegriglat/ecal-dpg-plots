b1c813894c7ba5acb3cb330f062dd00a64d268cc
Grigory <thegriglat@gmail.com>
2020-09-29 20:27:28 +0300

    Add abstract and icds

--- gautier_parser.py
+++ gautier_parser.py
@@ -9,8 +9,10 @@ import yaml
 from urllib2 import urlopen
 from urlparse import urljoin
 
+
 def read_site_content(url):
     return str(urlopen(url).read())
+
 
 class SessionParser(HTMLParser):
     url = ""
@@ -20,10 +22,12 @@ class SessionParser(HTMLParser):
     isDate = False
     cells = []
     lnkN = 0
+
     def dump(self, url):
         self.url = url
         self.feed(read_site_content(url))
         return self.cells
+
     def handle_starttag(self, tag, attrs):
         if tag == "td":
             self.isTd = True
@@ -35,8 +39,10 @@ class SessionParser(HTMLParser):
             self.isLink = True
             if len(filter(lambda x: x[0] == "href", attrs)) != 0:
                 self.cells.append({})
-                self.cells[len(self.cells) - 1]["url"] = urljoin(self.url, filter(lambda x: x[0] == "href", attrs)[0][1])
+                self.cells[len(self.cells) - 1]["url"] = urljoin(self.url,
+                                                                 filter(lambda x: x[0] == "href", attrs)[0][1])
                 self.lnkN = 1
+
     def handle_endtag(self, tag):
         if tag == "a":
             self.isLink = False
@@ -45,6 +51,7 @@ class SessionParser(HTMLParser):
             self.isTd = False
             self.isTitle = False
             self.isDate = False
+
     def handle_data(self, data):
         if self.isLink and self.lnkN == 1:
             self.cells[len(self.cells) - 1]["session"] = data
@@ -53,32 +60,51 @@ class SessionParser(HTMLParser):
         if self.isDate:
             self.cells[len(self.cells) - 1]["date"] = data.strip()
 
+
 class PlotParser(HTMLParser):
     url = ""
     isFigure = False
     isTitle = False
     cells = []
     data = ""
+    abstract = ""
+    icdslink = ""
+
     def dump(self, url):
         # sys.stderr.write("Dumping {0} ...\n".format(url))
         self.cells = []
         self.isTitle = False
         self.isTitle = False
+        self.isAbstract = False
         self.url = url
         self.feed(read_site_content(url))
-        return self.cells
+        return {
+            "plots": self.cells,
+            "abstract": self.abstract,
+            "icds": self.icdslink
+        }
+
     def handle_starttag(self, tag, attrs):
         if tag == "td" and filter(lambda x: x[0] == "class" and x[1] == "figure", attrs):
             self.isFigure = True
         if tag == "td" and filter(lambda x: x[0] == "class" and x[1] == "legend", attrs):
             self.isTitle = True
+        if tag == "td" and filter(lambda x: x[0] == "class" and x[1] == "textline", attrs):
+            self.isAbstract = True
+        if tag == "a":
+            if len(filter(lambda x: x[0] == "href" and "cds" in x[1], attrs)) != 0:
+                self.icdslink = filter(
+                    lambda x: x[0] == "href" and "cds" in x[1], attrs)[0][1]
         if tag == "img" and self.isFigure:
             if len(filter(lambda x: x[0] == "src", attrs)) != 0:
                 self.cells.append({})
-                self.cells[len(self.cells) - 1]["src"] = urljoin(self.url, filter(lambda x: x[0] == "src", attrs)[0][1])
+                self.cells[len(self.cells) - 1]["src"] = urljoin(self.url,
+                                                                 filter(lambda x: x[0] == "src", attrs)[0][1])
+
     def handle_endtag(self, tag):
         if tag == "td":
             self.isFigure = False
+            self.isAbstract = False
             if self.isTitle:
                 if not self.cells:
                     self.cells = [{}]
@@ -86,11 +112,14 @@ class PlotParser(HTMLParser):
                 self.data = ""
             self.isTitle = False
 
-
     def handle_data(self, data):
         if self.isTitle:
-            tmp = filter(lambda x: len(x) != 0, map(lambda x: x.strip(), data.split("<br>")))
+            tmp = filter(lambda x: len(x) != 0, map(
+                lambda x: x.strip(), data.split("<br>")))
             self.data = self.data + ' '.join(tmp)
+        if self.isAbstract:
+            self.abstract = data
+
 
 if len(sys.argv) < 2:
     print "usage: {0} url".format(sys.argv[0])
@@ -100,27 +129,20 @@ sessions = SessionParser().dump(sys.argv[1])
 for session in sessions:
     fldr = session["session"]
     os.mkdir(fldr)
-    with open(os.path.join(fldr, "metadata.yaml"), 'w') as fh:
-        yaml.safe_dump(
-            {
-                "title": session["title"],
-                "abstract": "abstract",
-                "date": session["date"],
-                "CDS": "CDS",
-                "iCMS": "iCMS"
-            },
-            fh
-        )
     parser = PlotParser()
     plots = parser.dump(session["url"])
-    for plot in plots:
+    abstract = plots["abstract"]
+    icds = plots["icds"]
+    for plot in plots["plots"]:
         if "src" not in plot:
-            sys.stderr.write("bad session: {0} | {1}\n".format(session["session"], json.dumps(plot)))
+            sys.stderr.write("bad session: {0} | {1}\n".format(
+                session["session"], json.dumps(plot)))
             continue
         src = plot["src"]
         pfldr = os.path.join(fldr, os.path.basename(src)).replace(".png", "")
         os.mkdir(pfldr)
-        print "wget -q -O {0}/{1} {2}".format(pfldr, os.path.basename(src), src)
+        print "wget -q -O {0}/{1} {2}".format(pfldr,
+                                              os.path.basename(src), src)
         with open(os.path.join(pfldr, "metadata.yaml"), 'w') as fh:
             yaml.safe_dump(
                 {
@@ -131,3 +153,14 @@ for session in sessions:
                 },
                 fh
             )
+    with open(os.path.join(fldr, "metadata.yaml"), 'w') as fh:
+        yaml.safe_dump(
+            {
+                "title": session["title"],
+                "abstract": abstract,
+                "date": session["date"],
+                "CDS": icds,
+                "iCMS": ""
+            },
+            fh
+        )
