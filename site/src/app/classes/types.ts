60cbade9a37b4d9421886a52fc6b791e04922100
Grigory <thegriglat@gmail.com>
2020-10-02 11:04:58 +0300

    Allow multiple images in directory

--- site/src/app/classes/types.ts
+++ site/src/app/classes/types.ts
@@ -1,15 +1,59 @@
-export interface Plot {
+export interface PlotData {
     session: string;
     name: string;
     caption: string;
     title: string;
     tags: string[];
-    formats: string[];
     date: string;
-    png: string;
-    pdf?: string;
-    jpg?: string;
-    root?: string;
+    files: string[];
+}
+
+export class Plot {
+    private _data: PlotData;
+    constructor(data: PlotData) {
+        this._data = data;
+    }
+
+    get data(): PlotData {
+        return this._data;
+    }
+
+    get session(): string {
+        return this._data.session;
+    }
+
+    get name(): string {
+        return this._data.name;
+    }
+
+    get caption(): string {
+        return this._data.caption;
+    }
+
+    get title(): string {
+        return this._data.title;
+    }
+
+    get date(): string {
+        return this._data.date;
+    }
+
+    get tags(): string[] {
+        return this._data.tags;
+    }
+
+    get png(): string[] {
+        return this.files('png');
+    }
+
+    get pdf(): string[] {
+        return this.files('pdf');
+    }
+
+    public files(ext?: string): string[] {
+        if (ext === undefined) { return this._data.files; }
+        return this._data.files.filter(f => f.slice(f.length - 3) === ext);
+    }
 }
 
 export interface Session {
@@ -31,7 +75,7 @@ export const AnySession = {
 } as Session;
 
 export interface Data {
-    plots: Plot[];
+    plots: PlotData[];
     sessions: Session[];
     builddate: string;
     commit: string;
