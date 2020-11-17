888f727e35e857e22e5fafc91a96b7c4df2ef678
Grigory Latyshev <thegriglat@gmail.com>
2020-11-17 14:32:57 +0300

    lfs

--- fill_content.py
+++ fill_content.py
@@ -13,7 +13,8 @@ for section in settings["sections"]:
     reponame = os.path.basename(git).split('.')[0]
     repodir = reponame + "_tmp"
     print "git clone {0} {1}".format(git, repodir)
+    print "cd {0}; git lfs fetch; cd -".format(repodir)
     print "cp -r {0}/content {1}".format(repodir, reponame)
-    print "cd - ".format(reponame)
+    print "cd .. ".format(reponame)
     print "python merge_content.py content/{0} > site/src/assets/{1}".format(reponame, section["file"])
     print "cp -r content/{0} site/src/assets/content/".format(reponame)
