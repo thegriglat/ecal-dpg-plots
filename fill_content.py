893fb63023fb59c2ecd2c58744b5a1bf9897c525
Dmitri Konstantinov <dmitri.konstantinov@cern.ch>
2021-03-01 17:09:29 +0100

    Fix for several repos

--- fill_content.py
+++ fill_content.py
@@ -12,9 +12,11 @@ for section in settings["sections"]:
     git = section["git"]
     reponame = os.path.basename(git).split('.')[0]
     repodir = reponame + "_tmp"
+    print "cur_dir=`pwd`"
     print "git clone {0} {1}".format(git, repodir)
     print "cd {0}; git lfs fetch; cd -".format(repodir)
     print "cp -r {0}/content {1}".format(repodir, reponame)
     print "cd .. ".format(reponame)
     print "python merge_content.py content/{0} > site/src/assets/{1}".format(reponame, section["file"])
     print "cp -r content/{0} site/src/assets/content/".format(reponame)
+    print "cd ${cur_dir}"
